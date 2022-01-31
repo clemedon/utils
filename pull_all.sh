fb57fc550ec8fd603aa3ed7f050c8aae658843f6
Clement VIDON <cvidon@student.42.fr>
2022-01-31 08:23:15 +0100

    Update scripts

--- pull_all.sh
+++ pull_all.sh
@@ -1,1 +1,6 @@
-find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin main \;
+# find . -type d -depth 1 -printf '%p' -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin main \;
+DIR=*
+for d in $DIR
+do
+    git --git-dir=$d/.git --work-tree=$PWD/$d pull origin main
+done
--- push_all.sh
+++ push_all.sh
@@ -1,1 +1,6 @@
-find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} push origin main \;
+# find . -type d -depth 1 -printf '%p' -exec git --git-dir={}/.git --work-tree=$PWD/{} push origin main \;
+DIR=*
+for d in $DIR
+do
+    git --git-dir=$d/.git --work-tree=$PWD/$d push origin main
+done
